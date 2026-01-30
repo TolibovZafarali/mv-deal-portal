ea325b09e55351af396e8cb7fe50c6354a4e3232
Zafarali Tolibov <zafaralit67@gmail.com>
2026-01-30 08:59:25 -0600

    chore(repo): enable property specifications

--- backend/src/main/java/com/megna/backend/repositories/PropertyRepository.java
+++ backend/src/main/java/com/megna/backend/repositories/PropertyRepository.java
@@ -5,10 +5,11 @@ import com.megna.backend.enums.ExitStrategy;
 import com.megna.backend.enums.OccupancyStatus;
 import com.megna.backend.enums.PropertyStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface PropertyRepository extends JpaRepository<Property, Long> {
+public interface PropertyRepository extends JpaRepository<Property, Long>, JpaSpecificationExecutor<Property> {
     List<Property> findByStatus(PropertyStatus status);
 
     List<Property> findByStatusOrderByCreatedAtDesc(PropertyStatus status);
