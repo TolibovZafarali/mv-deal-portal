d24778f7b966b6b847544f8886d8d48fc7f9d932
Zafarali Tolibov <zafaralit67@gmail.com>
2026-01-30 13:02:25 -0600

    chore(repository): remove unused queries and enable investor specifications

--- backend/src/main/java/com/megna/backend/repositories/InvestorRepository.java
+++ backend/src/main/java/com/megna/backend/repositories/InvestorRepository.java
@@ -1,22 +1,11 @@
 package com.megna.backend.repositories;
 
 import com.megna.backend.entities.Investor;
-import com.megna.backend.enums.InvestorStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface InvestorRepository extends JpaRepository<Investor, Long> {
+public interface InvestorRepository extends JpaRepository<Investor, Long>, JpaSpecificationExecutor<Investor> {
     Optional<Investor> findByEmail(String email);
-
-    List<Investor> findByStatus(InvestorStatus status);
-
-    List<Investor> findByStatusOrderByApprovedAtDesc(InvestorStatus status);
-
-    List<Investor> findByLastNameIgnoreCase(String lastName);
-
-    List<Investor> findByFirstNameIgnoreCaseAndLastNameIgnoreCase(String firstName, String lastName);
-
-    boolean existsByEmail(String email);
 }
