7b40dd857887613c5fb3ac0eb26bb98d7de2dd93
Zafarali Tolibov <zafaralit67@gmail.com>
2026-02-12 11:07:28 -0600

    chore: wrap app with BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import './styles/reset.css'
+import './styles/globals.css'
+
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
